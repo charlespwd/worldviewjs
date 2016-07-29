5f3fe2798b8450fa841bdbfb7fdd9905ab1c0bcb
Charles-P. Clermont <charles.pclermont@gmail.com>
2016-07-29 16:56:36 -0400

    Add zoom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,97 @@
-export function add(x, y) {
-  return x + y;
+const scaleVector = (k, v) => v.map(x => k * x);
+const addVectors = (v, u) => v.map((x, i) => x + u[i]);
+const minusVectors = (v, u) => v.map((x, i) => x - u[i]);
+const pointer = ({ pageX, pageY }) => [pageX, pageY];
+
+export default function PanZoom(render /* , opts */ ) {
+  let zoom = 1;
+  let containerOrigin_document = [0, 0]; // default, the position of the origin of the unscaled coords
+  let worldOrigin_container = [0, 0]; // default, the position of the origin of the scaled coords relative to the container
+  let worldSize = [0, 0];
+
+  const flow = (f, g) => (x) => g(f(x))
+  const fromDocumentToContainer = (vector_document) => (
+    minusVectors(vector_document, containerOrigin_document)
+  )
+  const fromContainerToDocument = (vector_container) => (
+    addVectors(vector_container, containerOrigin_document)
+  )
+  const fromWorldToContainer = (vector_world) => (
+    addVectors(scaleVector(zoom, vector_world), worldOrigin_container)
+  )
+  const fromContainerToWorld = (vector_container) => (
+    scaleVector(1 / zoom, minusVectors(vector_container, worldOrigin_container))
+  )
+  const fromWorldToDocument = flow(fromWorldToContainer, fromContainerToDocument)
+  const fromDocumentToWorld = flow(fromDocumentToContainer, fromContainerToWorld)
+
+  const transform = {
+    fromContainerToDocument,
+    fromContainerToWorld,
+    fromDocumentToContainer,
+    fromDocumentToWorld,
+    fromWorldToContainer,
+    fromWorldToDocument,
+  };
+
+  return {
+    debug,
+    transform,
+    setDimensions,
+    setContainerOrigin,
+    zoomTo,
+  };
+
+  function debug() {
+    return {
+      worldOrigin_container,
+      center_world,
+      centerWorld_document,
+      centerWorld_container,
+    }
+  }
+
+  function center_world() {
+    return scaleVector(1/2, worldSize);
+  }
+
+  function centerWorld_document() {
+    return transform.fromWorldToDocument(center_world());
+  }
+
+  function centerWorld_container(argument) {
+    return transform.fromWorldToContainer(center_world());
+  }
+
+  function setDimensions(worldWidth, worldHeight) {
+    worldSize = [worldWidth, worldHeight];
+  }
+
+  function setContainerOrigin(x, y) {
+    containerOrigin_document = [x, y];
+  }
+
+  // When zooming at a fixed point in the container
+  function zoomTo(e = {} , newZoom) {
+    const pointer_document = typeof e.pageX === 'number'
+      ? pointer(e)
+      : centerWorld_document();
+    const pointer_container = (
+      transform.fromDocumentToContainer(pointer_document)
+    );
+
+    const deltaZoom = zoom - newZoom;
+    zoom = newZoom;
+
+    worldOrigin_container = minusVectors(
+      scaleVector(deltaZoom, pointer_container),
+      worldOrigin_container
+    );
+
+    render(worldOrigin_container, zoom);
+  }
+
+  function panStart() {
+
+  }
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,116 @@
-import expect from 'expect';
-import { add } from '../src';
+import PanZoom from '../src';
+import { expect } from 'chai';
+import sinon from 'sinon';
 
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+const pointer = (pageX, pageY) => ({ pageX, pageY });
+
+describe('Module: PanZoom', () => {
+  let zoom, renderSpy, t;
+
+  beforeEach(() => {
+    renderSpy = sinon.spy();
+    zoom = new PanZoom(renderSpy);
+    zoom.setDimensions(100, 100)
+    t = zoom.transform;
+  })
+
+  describe('Unit: transform', () => {
+    it('fromDocumentToContainer should work', () => {
+      expect(t.fromDocumentToContainer([50, 50])).to.eql([50,50]);
+      zoom.setContainerOrigin(1, 1);
+      expect(t.fromDocumentToContainer([50, 50])).to.eql([49, 49])
+      zoom.zoomTo(undefined, 2);
+      expect(t.fromDocumentToContainer([50, 50])).to.eql([49, 49])
+    });
+
+    it('fromContainerToDocument should work', () => {
+      expect(t.fromContainerToDocument([50, 50])).to.eql([50, 50]);
+      zoom.setContainerOrigin(1, 1);
+      expect(t.fromContainerToDocument([49, 49])).to.eql([50, 50])
+      zoom.zoomTo(undefined, 2);
+      expect(t.fromContainerToDocument([49, 49])).to.eql([50, 50])
+    });
+
+    it('fromWorldToContainer should work', () => {
+      expect(t.fromWorldToContainer([50, 50])).to.eql([50, 50]);
+
+      zoom.setContainerOrigin(1, 1);
+      expect(t.fromWorldToContainer([50, 50])).to.eql([50, 50])
+
+      zoom.setContainerOrigin(0, 0);
+      expect(zoom.debug().worldOrigin_container).to.eql([0, 0]);
+
+      zoom.zoomTo(pointer(50, 50), 2);
+      expect(zoom.debug().worldOrigin_container).to.eql([-50, -50]);
+      expect(t.fromWorldToContainer([50, 50])).to.eql([50, 50])
+
+      zoom.setContainerOrigin(1, 1);
+      expect(t.fromWorldToContainer([50, 50])).to.eql([50, 50])
+    });
+
+    it('fromContainerToWorld should work', () => {
+      expect(t.fromContainerToWorld([50, 50])).to.eql([50, 50]);
+
+      zoom.setContainerOrigin(1, 1);
+      expect(t.fromContainerToWorld([50, 50])).to.eql([50, 50]);
+
+      zoom.setContainerOrigin(0, 0);
+      zoom.zoomTo(pointer(0, 0), 2)
+      expect(t.fromContainerToWorld([50, 50])).to.eql([25, 25])
+
+      zoom.setContainerOrigin(1, 1);
+      expect(t.fromContainerToWorld([50, 50])).to.eql([25, 25])
+    });
+
+    it('fromWorldToDocument should work', () => {
+      zoom.setContainerOrigin(200, 200);
+      expect(t.fromWorldToDocument([50, 50])).to.eql([250, 250]);
+
+      zoom.zoomTo(pointer(250, 250), 2);
+      expect(t.fromWorldToDocument([50, 50])).to.eql([250, 250]);
+    });
+
+    it('fromDocumentToWorld should work', () => {
+      zoom.setContainerOrigin(200, 200);
+      expect(t.fromDocumentToWorld([250, 250])).to.eql([50, 50]);
+
+      zoom.zoomTo(pointer(250, 250), 2);
+      expect(t.fromDocumentToWorld(zoom.debug().centerWorld_document())).to.eql([50, 50])
+    });
+  });
+
+  it('should give the coordinates of the center of the world', () => {
+    expect(zoom.debug().centerWorld_document()).to.eql([50, 50]);
+  });
+
+  it('should give the coordinates of the center of the moved world', () => {
+    expect(zoom.debug().centerWorld_document()).to.eql([50, 50]);
+    zoom.setContainerOrigin(200, 0);
+    expect(zoom.debug().centerWorld_document()).to.eql([250, 50]);
+  });
+
+  it('should render the transformation respective to container', () => {
+    zoom.setContainerOrigin(200, 200);
+    zoom.zoomTo(pointer(250, 250), 2); // offset by -50, -50, origin should be at 150, 150
+    expect(renderSpy).to.have.been.calledWith([-50, -50], 2);
+  });
+
+  it('should zoom about the origin', () => {
+    zoom.zoomTo(pointer(0, 0), 2);
+    expect(renderSpy).to.have.been.calledWith([ 0, 0 ], 2)
+  });
+
+  it('should do nothing when zoom is 0', () => {
+    zoom.zoomTo(pointer(0, 0), 1);
+    expect(renderSpy).to.have.been.calledWith([ 0, 0 ], 1)
+
+    // even if zoomed from somewhere
+    zoom.zoomTo(pointer(2, 2), 1);
+    expect(renderSpy).to.have.been.calledWith([ 0, 0 ], 1)
+  });
+
+  it('should zoom about the midpoint by default', () => {
+    zoom.zoomTo(undefined, 2);
+    expect(renderSpy).to.have.been.calledWith([ -50, -50 ], 2)
   });
 });
